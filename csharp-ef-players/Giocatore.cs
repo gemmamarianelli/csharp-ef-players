cf1bf9203c341daab456aebd5ebfe3a36d280cd1
Gemma Marianelli <115584815+gemmamarianelli@users.noreply.github.com>
2022-12-22 17:46:17 +0100

    Add project files.

---
+++ csharp-ef-players/Giocatore.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_ef_players
+{
+    [Table("giocatore")]
+    public class Giocatore
+    {
+        [Key]
+        public int GiocatoreId { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        [Column("Giocatore_Score")]
+        public int Score { get; set; }
+        [Column("Giocatore_Partite_Giocate")]
+        public int PartiteGiocate { get; set; }
+        [Column("Giocatore_Partite_Vinte")]
+        public int PartiteVinte { get; set; }
+    }    
+}
